bad59f9e6808a064e0d56a61d50874e8f3d4b7a4
AnotherCat <anothercat1259@gmail.com>
2020-12-09 21:12:32 +1300

    Add code

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # main.py
+from random import randint
+from time import sleep
+
+wait_time= 2
+send_emojis= ["🥳", "😀", "🎺", "🎉", "🎊"]
+
+
+def send_happy(i, time_to_wait):
+    emoji_string = ""
+    for a in range(0, i):
+        emoji_string = emoji_string + f" {send_emojis[randint(0,len(send_emojis)- 1)]} "
+    print(f"HAPPY BIRTHDAY {emoji_string}")
+    sleep(wait_time)
+    if time_to_wait < 0.05:
+        return 0.07
+    else:
+        return time_to_wait * 0.9
+
+
+while True:
+    for multiplier in send_emojis:
+        wait_time = send_happy(randint(0, 12), wait_time)
