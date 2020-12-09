0fac798dc6bbe3f2005f8802e5bdef4ab313f102
AnotherCat <anothercat1259@gmail.com>
2020-12-09 22:19:08 +1300

    Use correct check for ints

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,5 +2,5 @@ from main import send_happy
 
 
 def test_send_happy():
-    assert send_happy(5, 0.0005) is 0.07
-    assert send_happy(5, 1) is 0.9
+    assert send_happy(5, 0.0005) == 0.07
+    assert send_happy(5, 1) == 0.9
